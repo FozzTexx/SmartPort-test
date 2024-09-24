20cd30d1faa1444e7a8d9655b2caf0835d76bc8c
Chris Osborn <fozztexx@fozztexx.com>
2024-09-24 14:01:18 -0700

    Get count of SmartPort devices.

--- sp.exor.c
+++ sp.exor.c
@@ -7,12 +7,28 @@ typedef uint8_t *address;
 
 #include "callsp.h"
 
+enum {
+  SP_CMD_STATUS         = 0x00,
+  SP_CMD_READBLOCK      = 0x01,
+  SP_CMD_WRITEBLOCK     = 0x02,
+  SP_CMD_FORMAT         = 0x03,
+  SP_CMD_CONTROL        = 0x04,
+  SP_CMD_INIT           = 0x05,
+  SP_CMD_OPEN           = 0x06,
+  SP_CMD_CLOSE          = 0x07,
+  SP_CMD_READ           = 0x08,
+  SP_CMD_WRITE          = 0x09,
+};
+
 typedef struct {
   uint8_t count;
   uint8_t unit;
+  address status;
+  uint8_t type;
 } SmartPort_Parameters;
 
 address SmartPort_MLI;
+char buffer[20];
 
 address find_smartport()
 {
@@ -23,9 +39,9 @@ address find_smartport()
   for (slot = 7; slot > 0; slot--) {
     ptr = (uint8_t *) (0xc000 + slot * 256);
     if (*(ptr + 1) == 0x20 &&
-	*(ptr + 3) == 0x00 &&
-	*(ptr + 5) == 0x03 &&
-	*(ptr + 7) == 0x00) {
+        *(ptr + 3) == 0x00 &&
+        *(ptr + 5) == 0x03 &&
+        *(ptr + 7) == 0x00) {
       printf("Found SmartPort %i\n", slot);
       smartport = ptr + *(ptr + 0xff) + 3;
       break;
@@ -46,18 +62,32 @@ void main()
   SmartPort_MLI = find_smartport();
   printf("SmartPort: %04x\n", SmartPort_MLI);
 
-  parms.count = 1;
-  parms.unit = 0;
-
-  printf("%04x %x %04x\n", SmartPort_MLI, 5, &parms);
   for (err = count = 0; !err; count++) {
     if (kbhit()) {
       rcv = cgetc();
       if (rcv == 27)
-	break;
+        break;
     }
     
-    err = callsp(SmartPort_MLI, 5, (address) &parms);
+    parms.count = 1;
+    parms.unit = 0;
+    err = callsp(SmartPort_MLI, SP_CMD_INIT, (address) &parms);
+    if (err)
+      goto done;
+
+    parms.count = 3;
+    parms.unit = 0;
+    parms.status = (address) buffer;
+    parms.type = 0;
+    
+    err = callsp(SmartPort_MLI, SP_CMD_STATUS, (address) &parms);
+    if (!err) {
+      printf("  Num devices: %i\n", buffer[0]);
+      if (!buffer[0])
+        break;
+    }
+
+  done:
     printf("Count: %i  Error: %02x\n", count, err);
   }
 
